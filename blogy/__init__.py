723e4a4c2c6c9841be6b3aedf065a1304b4657cd
raj911tx <advancedroboticssystem@gmail.com>
2021-08-27 15:42:05 +0530

    basic

---
+++ blogy/__init__.py
@@ -0,0 +1,30 @@
+import os
+from flask import Flask
+
+def create_app(test_config=None):
+    #it is a application factory function
+    #creating app and configuring it
+    app=Flask(__name__,instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='DEV',
+        DATABASE=os.path.join(app.instance_path,'blogy.sqlite')
+    )
+
+    if test_config is None:
+        #loads the instance configuration
+        app.config.from_pyfile('config.py',silent=True)
+    else:
+        #loads it if passed in function parameter
+        app.config.from_mapping(test_config)
+
+    try:
+        #check if instance folder exists
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/')
+    def index():
+        return 'Hello World'
+
+    return app
