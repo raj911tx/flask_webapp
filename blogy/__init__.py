3d4e96961c4328725b7c6c6e14584e86f24f9c14
raj911tx <advancedroboticssystem@gmail.com>
2021-08-27 21:17:27 +0530

    authentication done

--- blogy/__init__.py
+++ blogy/__init__.py
@@ -1,12 +1,14 @@
 import os
 from flask import Flask
+from . import db
+from . import auth
 
 def create_app(test_config=None):
     #it is a application factory function
     #creating app and configuring it
     app=Flask(__name__,instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='DEV',
+        SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path,'blogy.sqlite')
     )
 
@@ -26,5 +28,8 @@ def create_app(test_config=None):
     @app.route('/')
     def index():
         return 'Hello World'
+
+    db.init_app(app)
+    app.register_blueprint(auth.bp)
 
     return app
